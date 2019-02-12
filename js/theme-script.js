afcf7bd952544fea8a1d99cc54ffb94e407c8a76
Aliaa Hosny <aliaahosny77@gmail.com>
2019-02-12 12:34:43 +0200

    finished comments

--- js/theme-script.js
+++ js/theme-script.js
@@ -68,8 +68,6 @@ $('.owl-carousel').each( function() {
 
   $carousel.owlCarousel({
     items : $carousel.data("items"),
-    animateOut: 'fadeOut',
-    animateIn: 'fadeInRight',
     slideBy : $carousel.data("slideby"),
     center : $carousel.data("center"),
     loop : true,
